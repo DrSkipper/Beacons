c2176d7ba9aa322524a8e9b97625bbcb953e0b16
Fletcher <fletcherc89@gmail.com>
2014-08-30 01:12:16 -0700

    Drawing a square

---
+++ Assets/Scripts/LevelGeneration/LevelVisualizer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelVisualizer : VoBehavior
+{
+	private bool _once;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		//TODO - fcole - This is temp execution logic
+		if (!_once)
+		{
+			_once = true;
+
+			//LevelGenerator generator = this.gameObject.GetComponent<LevelGenerator>();
+			//uint[,] map = generator.map.map;
+			//
+			//for (int x = 0; x < map
+
+			GameObject testObject = new GameObject();
+			SpriteRenderer testSprite = testObject.AddComponent<SpriteRenderer>();
+			testSprite.sprite = Resources.Load<Sprite>("Sprites/green_square");
+			//testObject.transform.position = transform.position;
+		}
+	}
+}
