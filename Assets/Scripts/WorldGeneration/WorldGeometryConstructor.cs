86dc0c0c80e2ba7edb221f30c83b9079d7eafdb8
Fletcher <fletcherc89@gmail.com>
2014-08-31 00:50:21 -0700

    Creating basic plane geometry based off world gen data

--- Assets/Scripts/WorldGeneration/WorldGeometryConstructor.cs
+++ Assets/Scripts/WorldGeneration/WorldGeometryConstructor.cs
@@ -3,156 +3,61 @@ using System.Collections;
 
 public class WorldGeometryConstructor : VoBehavior
 {
-	public float updateStepLength = 0.2f;
-	public int generationFramesPerUpdate = 2;
-	public bool runOnStartup = false;
-	public bool allowInterruption = false;
-	
+	public GameObject planePrefabA;
+	public GameObject planePrefabB;
+
 	// Use this for initialization
 	void Start()
 	{
 		_generator = this.gameObject.GetComponent<WorldGenerator>();
-		_generator.updateDelegate += this.mapWasUpdated;
-		if (this.runOnStartup)
-			this.restart();
+		_generator.clearMap();
+		_generator.runGenerationFrames(4096);
+		_generator.runGenerationFrames(4096);
+		setupMapDisplay();
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		if (_running)
-		{
-			if (_animatingLastUpdate)
-			{
-				updateAnimation();
-			}
-			else if (_mapWasUpdated)
-			{
-				_mapWasUpdated = false;
-				startAnimation();
-			}
-			else if (!_generationComplete)
-			{
-				_generator.runGenerationFrames(this.generationFramesPerUpdate);
-			}
-		}
-		else if (!this.allowInterruption && Input.GetKeyUp(KeyCode.Space))
-		{
-			this.restart();
-		}
-		
-		if (this.allowInterruption && Input.GetKeyUp(KeyCode.Space))
-			this.restart();
-	}
-	
-	public void mapWasUpdated()
-	{
-		_mapWasUpdated = true;
-		if (_generator.generationComplete)
-		{
-			_generationComplete = true;
-			_running = false;
-		}
-	}
-	
-	public void animationTimerCallback()
-	{
-		_animationTimer.paused = true;
-		_animatingLastUpdate = false;
-	}
-	
-	public void restart()
-	{
-		_generator.clearMap();
-		setupMapDisplay();
-		if (_animationTimer != null)
-			_animationTimer.invalidate();
-		_animationTimer = new Timer(this.updateStepLength, true, false, this.animationTimerCallback);
-		_animatingLastUpdate = false;
-		_mapWasUpdated = false;
-		_generationComplete = false;
-		_running = true;
 	}
 	
 	/**
 	 * Private
 	 */
-	private bool _running;
-	private bool _mapWasUpdated;
-	private bool _animatingLastUpdate;
-	private bool _generationComplete;
 	private WorldGenerator _generator;
-	
-	private Sprite _spriteA;
-	private Sprite _spriteB;
-	private SpriteRenderer[,] _tileRenderers;
-	private Timer _animationTimer;
-	
-	private void startAnimation()
-	{
-		_animatingLastUpdate = true;
-		_animationTimer.paused = false;
-		
-		WorldGenTile[,] map = _generator.map.map;
-		
-		int width = map.GetLength(0);
-		int height = map.GetLength(1);
-		
-		for (int x = 0; x < width; ++x)
-		{
-			for (int y = 0; y < height; ++y)
-			{
-				_tileRenderers[x, y].sprite = spriteForType(map[x, y].type);
-			}
-		}
-	}
-	
-	private void updateAnimation()
-	{
-		_animationTimer.update();
-	}
+	private GameObject[,] _tileObjects;
 	
 	private void setupMapDisplay()
 	{
-		if (_tileRenderers != null)
-		{
-			for (int x = 0; x < _tileRenderers.GetLength(0); ++x)
-			{
-				for (int y = 0; y < _tileRenderers.GetLength(1); ++y)
-				{
-					Destroy(_tileRenderers[x, y].gameObject);
-				}
-			}
-		}
-		
 		WorldGenTile[,] map = _generator.map.map;
-		
-		_spriteA = Resources.Load<Sprite>("Sprites/blue_square");
-		_spriteB = Resources.Load<Sprite>("Sprites/yellow_square");
 		
 		int width = map.GetLength(0);
 		int height = map.GetLength(1);
 		int halfWidth = width / 2;
 		int halfHeight = height / 2;
+		Bounds tileBounds = this.planePrefabA.renderer.bounds;
 		
-		_tileRenderers = new SpriteRenderer[width, height];
+		_tileObjects = new GameObject[width, height];
 		
 		for (int x = 0; x < width; ++x)
 		{
 			for (int y = 0; y < height; ++y)
 			{
-				GameObject go = new GameObject();
-				SpriteRenderer sprite = go.AddComponent<SpriteRenderer>();
-				sprite.sprite = spriteForType(map[x, y].type);
-				Bounds bounds = sprite.bounds;
-				go.transform.position = new Vector3((x - halfWidth) * bounds.size.x, (y - halfHeight) * bounds.size.y, 0);
-				_tileRenderers[x, y] = sprite;
+				Vector3 position = new Vector3((x - halfWidth) * tileBounds.size.x, 0, (y - halfHeight) * tileBounds.size.z);
+				_tileObjects[x, y] = Instantiate(objectForType(map[x, y].type), position, Quaternion.identity) as GameObject;
 			}
 		}
 	}
 	
-	private Sprite spriteForType(uint type)
+	private GameObject objectForType(uint type)
 	{
-		return type == WorldGenerator.TILE_TYPE_A ? _spriteA : _spriteB;
+		if (type == WorldGenerator.TILE_TYPE_A)
+		{
+			return this.planePrefabA;
+		}
+		else
+		{
+			return this.planePrefabB;
+		}
 	}
 }
